b654e0ec70920d7ffd2db305f773fe833ee6b21c
SJuras <jurasstpko@gmailcom>
2022-01-25 12:24:07 +0100

    v1.1.5 testing the buying functionality.

--- solariexchange/src/contracts/EthSwap.sol
+++ solariexchange/src/contracts/EthSwap.sol
@@ -12,6 +12,14 @@ contract EthSwap {
   Token public token;
   uint public rate = 100;
 
+  // events
+  event TokenPurchased(
+    address account,
+    address token,
+    uint amount,
+    uint rate
+    );
+
   constructor(Token _token) public {
     token = _token;
   }
@@ -21,7 +29,15 @@ contract EthSwap {
     // amount of Ether * Redemption rate (num of received tokens for 1 ether)
     // calc tokens to buy (100 tokens for 1 ether);
     uint tokenAmount = msg.value * rate;
+
+    // require that exchange has enough tokens to sell
+    require(token.balanceOf(address(this)) >= tokenAmount);
+
+    // transfer tokens to user 
     token.transfer(msg.sender, tokenAmount);
+
+    // emit event that tokens were purchased
+    emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
   }
 
 }
